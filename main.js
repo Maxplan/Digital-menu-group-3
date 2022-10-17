20db96d075bec563dfe67a18382c34724f644a9c
RamyHajMousa <hara22cg@student.ju.se>
2022-10-17 11:31:06 +0200

    Removed popup menu's code from main.js to popup.js

--- main.js
+++ main.js
@@ -83,31 +83,3 @@ function createCards() {
 }
 
 createCards();
-
-// ======================== POPUP MENU ========================
-let popupHeader = document.querySelector(".popupHeader");
-let popupPrice = document.querySelector(".popupPrice");
-let popupImg = document.querySelector(".popupImg");
-let popupText = document.querySelector(".popupText");
-
-// Add content to popup menu
-function addPopupContent(cardNumber) {
-    popupHeader.innerHTML = recipe[cardNumber].title;
-    popupPrice.innerHTML = recipe[cardNumber].price + " KR";
-    popupImg.setAttribute("src", recipe[cardNumber].image);
-    popupText.innerHTML = recipe[cardNumber].description;
-}
-
-
-
-//open popup menu
-const POPUP = document.getElementById("popup");
-
-function openPopup(cardNumber) {
-    POPUP.classList.add("openPopup");
-    addPopupContent(cardNumber);
-}
-// Close popup menu
-function closePopup() {
-    POPUP.classList.remove("openPopup");
-}
